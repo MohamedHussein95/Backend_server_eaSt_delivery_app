561ec71ac7271daad18343dab1aaf5bc17cbff31
ckjunior <124371707+MohamedHussein95@users.noreply.github.com>
2023-05-05 23:27:03 +0300

    removed host from  the server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const connectDB = require('./config/db');
 
 const app = express();
 const port = process.env.PORT || 8001;
-const host = process.env.HOST || 'http://localhost';
+//const host = process.env.HOST || 'http://localhost';
 
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
@@ -18,6 +18,4 @@ app.get('/', (req, res) => {
 
 connectDB();
 
-app.listen(port, host, () =>
-	console.log(`server is listening on port:${port} on host:${host}`)
-);
+app.listen(port, () => console.log(`server is listening on port:${port} `));
